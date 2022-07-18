33e4d0032b1fafb0d2f307a58c9337572ce5bf52
cuifuKim <77271693+cuifuKim@users.noreply.github.com>
2022-07-19 01:42:03 +0800

    Update sm3.cu

--- sm3.cu
+++ sm3.cu
@@ -1,8 +1,3 @@
-#include "sm3.h"
-#include <string.h>
-#include "stdio.h"
-#include <time.h>
-
 #define BLOCK_SIZE 256
 
 #define F1(x,y,z)(((x)^(y)^(z)))
@@ -12,40 +7,28 @@
 #define P0(x)x^R(x,9)^R(x,17)
 #define P1(x)x^R(x,15)^R(x,23)
 
-__device__
-unsigned int bswap32(unsigned int x){
+__device__ unsigned int bswap32(unsigned int x){
   unsigned int result;
   unsigned char* out =(unsigned char *)&result;
   unsigned char* input = (unsigned char *)&x;
-  out[3] = input[0];
-  out[2] = input[1];
-  out[1] = input[2];
-  out[0] = input[3];
+  out[3] = input[0];  out[2] = input[1];
+  out[1] = input[2];  out[0] = input[3];
   return result;
 }
 
-__device__
-unsigned long bswap64(unsigned long x){
+__device__  unsigned long bswap64(unsigned long x){
   unsigned long result;
   unsigned char* out = (unsigned char *)&result;
   unsigned char* input = (unsigned char *)&x;
 
-  out[7] = input[0];
-  out[6] = input[1];
-  out[5] = input[2];
-  out[4] = input[3];
-  out[3] = input[4];
-  out[2] = input[5];
-  out[1] = input[6];
-  out[0] = input[7];
+  out[7] = input[0];out[6] = input[1];
+  out[5] = input[2];out[4] = input[3];
+  out[3] = input[4];out[2] = input[5];
+  out[1] = input[6];out[0] = input[7];
   return result;
 }
 
-__device__
-
-W R_replace(W v,int n){
-
-  
+__device__ W R_replace(W v,int n){
   int right_shift = 32 - n;
   n = n % 32;
   right_shift = (right_shift + 32) % 32;
@@ -58,10 +41,7 @@ W R_replace(W v,int n){
   return r;
 }
 
-__device__
-
-W R(W v,int n){
-
+__device__ W R(W v,int n){
   int right_shift = 32 - n;
   n = n % 32;
   right_shift = (right_shift + 32) % 32;
@@ -72,76 +52,51 @@ W R(W v,int n){
   
   return r;
 }
-__device__ 
-void sm3_compress(sm3_ctx *c) {
+
+__device__ void sm3_compress(sm3_ctx *c) {
     W t1,t2,i,t,s1,s2,x[8],w[68];
 
-    // load data
     F(16)w[i]=rev32(c->x.w[i]);
 
-    for(i=16;i<68;i++)
-      w[i]=P1(w[i-16]^w[i-9]^R(w[i-3],15))^R(w[i-13],7)^w[i- 6];
+    for(i=16;i<68;i++)  w[i]=P1(w[i-16]^w[i-9]^R(w[i-3],15))^R(w[i-13],7)^w[i- 6];
 
-    // load internal state
     F(8)x[i]=c->s[i];
     
-    // compress data
     F(64) {
       t=(i<16)?0x79cc4519:0x7a879d8a;         
       s2=R(x[0],12);      
       s1=R(s2+x[4]+R(t,i),7);
       s2^=s1;
-      if(i<16) {
-        t1=F1(x[0],x[1],x[2])+x[3]+s2+(w[i]^w[i+4]);
-        t2=F1(x[4],x[5],x[6])+x[7]+s1+w[i];
-      } else {
-        t1=FF(x[0],x[1],x[2])+x[3]+s2+(w[i]^w[i+4]);
-        t2=GG(x[4],x[5],x[6])+x[7]+s1+w[i];      
-      }
-      x[3]=x[2];x[2]=R(x[1],9);x[1]=x[0];x[0]=t1;
-      x[7]=x[6];x[6]=R(x[5],19);x[5]=x[4];x[4]=P0(t2);     
-    }
-
+      if(i<16) {  t1=F1(x[0],x[1],x[2])+x[3]+s2+(w[i]^w[i+4]);  t2=F1(x[4],x[5],x[6])+x[7]+s1+w[i]; } 
+      else     {  t1=FF(x[0],x[1],x[2])+x[3]+s2+(w[i]^w[i+4]);  t2=GG(x[4],x[5],x[6])+x[7]+s1+w[i]; }
+                  x[3]=x[2];x[2]=R(x[1],9);x[1]=x[0];x[0]=t1;   x[7]=x[6];x[6]=R(x[5],19);x[5]=x[4];x[4]=P0(t2); }
     F(8)c->s[i]^=x[i];
 }
 
-__device__ 
-void sm3_init(sm3_ctx *c) {    
-    c->s[0]=0x7380166f;
-    c->s[1]=0x4914b2b9;
-    c->s[2]=0x172442d7;
-    c->s[3]=0xda8a0600;
-    c->s[4]=0xa96f30bc;
-    c->s[5]=0x163138aa;
-    c->s[6]=0xe38dee4d;
-    c->s[7]=0xb0fb0e4e;
+__device__  void sm3_init(sm3_ctx *c) {    
+    c->s[0]=0x7380166f; c->s[1]=0x4914b2b9;
+    c->s[2]=0x172442d7; c->s[3]=0xda8a0600;
+    c->s[4]=0xa96f30bc; c->s[5]=0x163138aa;
+    c->s[6]=0xe38dee4d; c->s[7]=0xb0fb0e4e;
     c->len =0;
 }
 
-__device__ 
-void sm3_update(sm3_ctx *c, const void *in, W len) {
+__device__  void sm3_update(sm3_ctx *c, const void *in, W len) {
     B *p=(B*)in;
     W i, idx;
-    
-    // index = len % 64
     idx = c->len & 63;
-    // update total length
     c->len += len;
     
     for (i=0;i<len;i++) {
-      // add byte to buffer
       c->x.b[idx]=p[i]; idx++;
-      // buffer filled?
       if(idx==64) {
-        // compress it
         sm3_compress(c);
         idx=0;
       }
     }
 }
 
-__device__ 
-void sm3_final(void *out, sm3_ctx *c) {
+__device__ void sm3_final(void *out, sm3_ctx *c) {
     W i,len,*p=(W*)out;
     
     // get index
