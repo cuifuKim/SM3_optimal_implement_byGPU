b23a032b5234657b5537fe8b9e3ca79657d3b478
cuifuKim <77271693+cuifuKim@users.noreply.github.com>
2022-07-19 01:46:16 +0800

    Update sm3.h

--- sm3.h
+++ sm3.h
@@ -1,12 +1,7 @@
 #ifndef SM3_H
 #define SM3_H
 
-//#define R(v,n)(((v)<<(n))|((v)>>(32-(n))))
 #define F(n)for(i=0;i<n;i++)
-
-// #define rev32(x) __builtin_bswap32(x)
-// #define rev64(x) __builtin_bswap64(x)
-
 
 #define rev32(x) bswap32(x)
 #define rev64(x) bswap64(x)
@@ -29,18 +24,9 @@ typedef struct _sm3_ctx {
 extern "C" {
 #endif
 
-
-
-__global__ 
-void gpu_sm3(void* d_m, int len_m ,unsigned char* d_h);
-__global__
-void find_valid_nounce(int n, const unsigned char* boundry, const char* msg, int msg_len);
+__global__ void gpu_sm3(void* d_m, int len_m ,unsigned char* d_h);
+__global__  void find_valid_nounce(int n, const unsigned char* boundry, const char* msg, int msg_len);
 void host_find_valid_nounce(Q N, const unsigned char* boundry, const char* msg);
-// void sm3_init(sm3_ctx *c);
-// void sm3_update(sm3_ctx *c, const void *in, W len);
-// void sm3_final(void *out,sm3_ctx *c);
-
-
 
 #ifdef __cplusplus
 }
